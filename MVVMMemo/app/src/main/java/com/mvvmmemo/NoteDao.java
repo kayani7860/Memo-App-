9cd739e48a0e06226fdece1170aedee6cf27cb48
Hammad Kayani <36533976+kayani7860@users.noreply.github.com>
2021-03-10 15:40:34 +0100

    Build1

---
+++ MVVMMemo/app/src/main/java/com/mvvmmemo/NoteDao.java
@@ -0,0 +1,18 @@
+package com.mvvmmemo;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+
+@Dao
+public interface NoteDao {
+    @Insert
+    void insert(Note note);
+    @Update
+    void update(Note note);
+    @Delete
+    void delete(Note note);
+    
+}
